a7f1502cd170dc0d7446153ab65911bbc1e2a8ce
Stanni11 <supercoder@SuperCoders-Air.fritz.box>
2022-08-03 20:44:14 +0200

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,30 +11,26 @@ const trenneAlles = (event) => {
     let trennen = document.forms[0].trennen.value
 
 
-    //if (pTag) { resultBox.removeChild(pTag) }
-
-
-    /*    if (index !== -1) {
-           outputVorne.innerHTML = "Dieses Wort ist nicht auffindbar Babe"
-       } else; */
-
     const vorTrennen = () => {
         let textdavor1 = txtInput.slice(0, index)
         outputVorne.innerHTML = textdavor1
-        let textdanach1 = txtInput.slice(index + signInput.length)
+        let textdanach1 = txtInput.slice(index)
         outputHinten.innerHTML = textdanach1
 
 
     }
     const danachTrennen = () => {
+        let textdavor2 = txtInput.slice(0, index + signInput.length)
+        outputVorne.innerHTML = textdavor2
         let textdanach2 = txtInput.slice(index + signInput.length)
         outputHinten.innerHTML = textdanach2
-        let textdavor2 = txtInput.slice(0, index)
-        outputVorne.innerHTML = textdavor2
 
 
     }
-    if (trennen === "davor") {
+    if (index === -1) {
+        outputVorne.innerHTML = "Dieses Wort ist nicht auffindbar Babe"
+
+    } else if (trennen === "davor") {
         vorTrennen()
 
     }
@@ -44,7 +40,3 @@ const trenneAlles = (event) => {
 }
 
 
-//ich raff das nicht : ---->
-
-/* senBefore = sentence.slice(0, searchResult + seperator.length)
-document.getElementById('textBefore').innerHTML = senBefore */
